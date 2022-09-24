35ce21552f18571db3f17dda8ac9866ec4838703
Jonathan Mojica <60447812+AsyncSpaghettiDev@users.noreply.github.com>
2022-09-24 11:42:20 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,7 +2,79 @@ const importantIcon = 'fas';
 const notImportantIcon = 'far';
 
 function saveTask() {
-    console.log("Button clicked");
+    // get data
+    let title = $("#task-name").val();
+    let dueDate = $("#due-date").val();
+    let description = $("#description").val();
+    let tag = $("#tag").val();
+    let color = $("#color").val();
+    let category = $("#category").val();
+    let important = $("#toggleImportance").hasClass(importantIcon);
+
+    let task = new Task(title, dueDate, description, tag, color, category, important);
+    // call created function
+    displayTask(task);
+    clearForm();
+}
+
+/**
+ * Clear form function
+ * and set date to today
+ */
+
+function clearForm() {
+    $("#task-name").val("");
+    let today = new Date();
+    $('#due-date').val(today.toISOString().substr(0, 10));
+    $("#description").val("");
+    $("#tag").val("");
+    $("#color").val("#9900ff");
+    $("#category").val("work");
+    $("#toggleImportance").removeClass(importantIcon);
+    $("#toggleImportance").addClass(notImportantIcon);
+}
+
+/**
+ * Create a display function
+ * the fn should accept a task object as a parameter
+ * console log the task object
+ */
+
+function displayTask(task) {
+    console.log(task);
+
+    // create div with class="task"
+    // Separate title and description of the task in a div with a class of "task_info"
+    // Separate the due date in a div with a class of "task_info"
+    // Separate the tag and the category in a div with a class of "task_info"
+    // Set the background color of the task to the color of the task using iline css
+    // Create all this using string template
+    // Do something with isImportant wonder me copilot
+    // Add the task to the tasks-list in the DOM
+
+    let html = `
+    <div class="task" style="background-color:${task.color}">
+        <div class="task_info">
+            <h3>${task.title}</h3>
+            <p>${task.description}</p>
+        </div>
+        <div class="task_info">
+            <p>Due Date</p>
+            <p>${task.dueDate}</p>
+        </div>
+        <div class="task_info">
+            <p>Tag: ${task.tag}</p>
+            <p>Category: ${task.category}</p>
+        </div>
+        <div class="task_info task_actions">
+            <i class="task_toggleImportance ${task.isImportant ? importantIcon : notImportantIcon} fa-star"></i> 
+            <i class="task_delete fas fa-trash-alt"></i>
+            <i class="task_edit fas fa-edit"></i>
+        </div>
+    </div>
+                `;
+
+    $("#tasks-list").append(html);
 }
 
 function toggleForm() {
@@ -21,6 +93,9 @@ function init() {
     console.log("Task Manager");
 
     // load prev data
+    $("#color").val("#9900ff");
+    let today = new Date();
+    $('#due-date').val(today.toISOString().substr(0, 10));
 
     // hook events
     $("#btnSave").click(saveTask);
